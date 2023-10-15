ebd98bd09a8ee0e2456e0b32f7057239e590ce86
YadokBlack <kodaygames@gmail.com>
2023-10-15 16:14:10 +0200

    Cambio declaracion de las variables de colores

--- Assets/Scripts/CambioColorLuz.cs
+++ Assets/Scripts/CambioColorLuz.cs
@@ -6,6 +6,13 @@ public class CambioColorLuz : MonoBehaviour
 
     public PasoDelTiempo tiempo;
 
+    [SerializeField]
+    private Color colorManyana = new Color(0.741f, 0.925f, 0.964f); // Azul claro para la mañana
+    [SerializeField]
+    private Color colorTarde = new Color(0.898f, 0.824f, 0.608f);  // Naranja para la tarde
+    [SerializeField]
+    private Color colorNoche = new Color(0.098f, 0.180f, 0.416f);  // Azul oscuro para la noche
+
     private void Update()
     {
         CambiarColorDeLuzDireccional();
@@ -13,12 +20,9 @@ public class CambioColorLuz : MonoBehaviour
 
     private void CambiarColorDeLuzDireccional()
     {
-        Color colorManana = new Color(0.741f, 0.925f, 0.964f); // Azul claro para la mañana
-        Color colorTarde = new Color(0.898f, 0.824f, 0.608f);  // Naranja para la tarde
-        Color colorNoche = new Color(0.098f, 0.180f, 0.416f);  // Azul oscuro para la noche
 
         float horaInterpolacion = Mathf.InverseLerp(0, 18, tiempo.horasDelJuego);
-        Color colorActual = Color.Lerp(colorManana, colorTarde, horaInterpolacion);
+        Color colorActual = Color.Lerp(colorManyana, colorTarde, horaInterpolacion);
 
         if (tiempo.horasDelJuego > 17)
         {
