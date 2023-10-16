ff9273aab6ede5f836456141f785a68c0cd76ff3
YadokBlack <kodaygames@gmail.com>
2023-10-16 12:13:25 +0200

    Se coloca el codigo quitando lineas vacias

--- Assets/Scripts/Reloj.cs
+++ Assets/Scripts/Reloj.cs
@@ -6,18 +6,14 @@ using TMPro;
 
 public class Reloj : MonoBehaviour
 {
-
     [Tooltip("Donde se muestra")]
-
     public TextMeshProUGUI currentTime;
-
 
     void FixedUpdate()
     {
         currentTime.color = Color.red;
         currentTime.text =
         System.DateTime.Now.Hour.ToString("00") + ":" +
-        System.DateTime.Now.Minute.ToString("00");
-     
+        System.DateTime.Now.Minute.ToString("00");     
     }
 }
