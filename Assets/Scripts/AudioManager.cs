6e8bac78e56c8f37bed3192d847431683fce1dd1
YadokBlack <kodaygames@gmail.com>
2023-10-18 15:48:14 +0200

    Quito comentarios en AudioManager.cs

--- Assets/Scripts/AudioManager.cs
+++ Assets/Scripts/AudioManager.cs
@@ -8,17 +8,15 @@ public class AudioManager : MonoBehaviour
 
     private void Start()
     {
-        // Obtén el componente AudioSource adjunto a este objeto
         audioSource = GetComponent<AudioSource>();
     }
 
     public void PlayAudioByIndex(int index)
     {
         if (audioSource == null) return;
-        // Asegúrate de que el índice sea válido
+
         if (index >= 0 && index < audioClips.Count)
         {
-            // Asigna el clip de audio al AudioSource y reproduce el sonido
             audioSource.clip = audioClips[index];
             audioSource.Play();
         }
