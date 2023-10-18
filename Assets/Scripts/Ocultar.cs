0633cc29ae881fad9077f27e38481743bca91e79
YadokBlack <kodaygames@gmail.com>
2023-10-18 16:24:37 +0200

    Quitar lineas vacias y comentarios en Ocultar.cs

--- Assets/Scripts/Ocultar.cs
+++ Assets/Scripts/Ocultar.cs
@@ -6,20 +6,16 @@ public class Ocultar : MonoBehaviour
 {
     private MeshRenderer meshRenderer;
 
-    // Start is called before the first frame update
     void Start()
     {
-        // Obtenemos el componente MeshRenderer del objeto
         meshRenderer = GetComponent<MeshRenderer>();
 
-        // Comprobamos si el objeto tiene un MeshRenderer
         if (meshRenderer == null)
         {
             Debug.LogWarning("El objeto no tiene un MeshRenderer.");
         }
         else
         {
-            // Ocultamos el objeto desactivando el componente MeshRenderer
             meshRenderer.enabled = false;
         }
     }
