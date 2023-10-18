2ce8a0698ca29d1adfa91d2f68d2863cc349ee03
YadokBlack <kodaygames@gmail.com>
2023-10-18 16:25:59 +0200

    Quitar comentarios en SalirConEsc.cs

--- Assets/Scripts/SalirConEsc.cs
+++ Assets/Scripts/SalirConEsc.cs
@@ -2,16 +2,12 @@ using UnityEngine;
 
 public class SalirConEsc : MonoBehaviour
 {
-    // Update is called once per frame
     void Update()
     {
-        // Verificar si la plataforma de ejecución es PC
         if (Application.platform == RuntimePlatform.WindowsPlayer || Application.platform == RuntimePlatform.OSXPlayer || Application.platform == RuntimePlatform.LinuxPlayer)
         {
-            // Verificar si se presiona la tecla ESC
             if (Input.GetKeyDown(KeyCode.Escape))
             {
-                // Salir del juego
                 Application.Quit();
             }
         }
