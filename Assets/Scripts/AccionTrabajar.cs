2d0a4fca4c242480938de6a963a2e529e2a79a4a
YadokBlack <kodaygames@gmail.com>
2023-10-28 10:35:09 +0200

    Cambio AccionTrabajar como una subclase de AccionBase

--- Assets/Scripts/AccionTrabajar.cs
+++ Assets/Scripts/AccionTrabajar.cs
@@ -3,14 +3,8 @@ using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 
-/*
- *  Similar a Accion pero con las cosas concretas del juego principal
- *  
- *  Al poner esto de arriba me indica que quizas pueda reducir o realizar 
- *  de otro modo accion trabajar para que utilice parte de acciones.
- */
 
-public class AccionTrabajar : MonoBehaviour
+public class AccionTrabajar : AccionBase
 {
     public string mensajeInteraccion = "Pulsa E para programar.";
 
@@ -24,37 +18,7 @@ public class AccionTrabajar : MonoBehaviour
     // pantalla de victoria
     public GameObject victoria;
     public GameObject pantallaFin;
-
-    private const float tiempoCompleto = 1.0f;
-
-    public Color colorPunto = Color.red;
-
-    public KeyCode teclaAccion = KeyCode.E;
-
-    public int costeTiempo = 5;
-    public float[] beneficios = null;
-
-    public GameObject objetoDestacado;
-    public Vector3 posicionObjeto;
-    private Vector3 escalaOriginal;
-    private bool enTransicion;
-    private float tiempoInicioTransicion;
-    public float duracionTransicion = 0.30f;
-    public float escalaPorcentaje;
-
-    public ControlZonas zonaControl;
-    public ZonaDeColision zonaControlada;
-
-    public PasoDelTiempo reloj;
-
-    public Condicion condicion;
-
-    [SerializeField]
-    private GameObject pantalla;
-
-    public AudioManager audioManager;
-    public int numClip=0;
-
+    
     public bool haGanado;
 
     public AudioClip sonidoAEjecutar; 
@@ -72,36 +36,6 @@ public class AccionTrabajar : MonoBehaviour
         if (sonidoAEjecutar == null)
         {
             Debug.LogWarning("No se ha asignado un AudioClip para reproducir.");
-        }
-    }
-
-    void Start()
-    {
-        if (objetoDestacado != null)
-        {
-            escalaOriginal = objetoDestacado.transform.localScale;
-        }
-        enTransicion = false;
-    }
-
-    public void AnimacionAgrandar()
-    {
-        if (enTransicion)
-        {
-            float tiempoTranscurrido = Time.time - tiempoInicioTransicion;
-            float fraccionDeTiempo = Mathf.Clamp01(tiempoTranscurrido / duracionTransicion);
-            Vector3 escalaDeseada = escalaOriginal * escalaPorcentaje;
-
-            objetoDestacado.transform.localScale = Vector3.Lerp(escalaOriginal, escalaDeseada, fraccionDeTiempo);
-
-            if (fraccionDeTiempo == tiempoCompleto)
-            {
-                enTransicion = false;
-            }
-        }
-        if (!enTransicion && objetoDestacado != null)
-        {
-            objetoDestacado.transform.localScale = escalaOriginal;
         }
     }
 
@@ -198,23 +132,5 @@ public class AccionTrabajar : MonoBehaviour
         AnimacionAgrandar();
 
         ControlPantallaEnZona();
-    }
-
-    private void ControlPantallaEnZona()
-    {
-        if (zonaControl.jugadorEnZona && zonaControl.nombreZonaJugador == zonaControlada.name && pantalla != null)
-        {
-            pantalla.SetActive(true);
-        }
-        else if (!zonaControl.jugadorEnZona && pantalla != null)
-        {
-            pantalla.SetActive(false);
-        }
-    }
-
-    private void OnDrawGizmosSelected()
-    {
-        Gizmos.color = colorPunto; // Color del punto
-        Gizmos.DrawSphere(posicionObjeto, 0.01f); // Dibujar un punto en la posición deseada
     }
 }
