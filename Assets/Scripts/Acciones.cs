a4deebad13def5a0b5ff50f77742cdc93ab170a9
YadokBlack <kodaygames@gmail.com>
2023-10-28 10:24:22 +0200

    Cambio la clase Acciones como  una subclase de AccionBase, quitando todo lo que se repite.

--- Assets/Scripts/Acciones.cs
+++ Assets/Scripts/Acciones.cs
@@ -3,50 +3,11 @@ using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 
-public class Acciones : MonoBehaviour
+public class Acciones : AccionBase
 {
-    private const float tiempoCompleto = 1.0f;
-
-    public Color colorPunto = Color.red;
-
-    public KeyCode teclaAccion = KeyCode.E;
-
-    public int costeTiempo = 5;
-    public float[] beneficios = null;
-
-    public GameObject objetoDestacado;
-    public Vector3 posicionObjeto;
-    public Vector3 escalaOriginal;
-    private bool enTransicion;
-    private float tiempoInicioTransicion;
-    public float duracionTransicion = 0.30f;
-    public float escalaPorcentaje;
-
-    public ControlZonas zonaControl;
-    public ZonaDeColision zonaControlada;
-
-    public PasoDelTiempo reloj;
-
-    public Condicion condicion;
-
-    [SerializeField]
-    private GameObject pantalla;
-
-    public AudioManager audioManager;
-    public int numClip=0;
-
     private float alturaMaxima = 0f;
 
     public AccionTrabajar accionTrabajo;
-
-    void Start()
-    {
-        if (objetoDestacado != null)
-        {
-            escalaOriginal = objetoDestacado.transform.localScale; 
-        }
-        enTransicion = false;
-    }
 
     private float ObtenerAlturaMaxima(Transform objetoPadre)
     {
@@ -69,27 +30,6 @@ public class Acciones : MonoBehaviour
             alturaObjetoPadre = Mathf.Max(alturaObjetoPadre, alturaHijoRecursiva);
         }
         return alturaObjetoPadre;
-    }
-
-    public void AnimacionAgrandar()
-    {
-        if (enTransicion)
-        { 
-            float tiempoTranscurrido = Time.time - tiempoInicioTransicion;
-            float fraccionDeTiempo = Mathf.Clamp01(tiempoTranscurrido / duracionTransicion);
-            Vector3 escalaDeseada = escalaOriginal * escalaPorcentaje;
-
-            objetoDestacado.transform.localScale = Vector3.Lerp(escalaOriginal, escalaDeseada, fraccionDeTiempo);
-
-            if (fraccionDeTiempo == tiempoCompleto)
-            {
-                enTransicion = false;
-            }
-        }
-        if (!enTransicion && objetoDestacado != null)
-        {
-            objetoDestacado.transform.localScale = escalaOriginal;
-        }
     }
 
     void Update()
@@ -118,23 +58,5 @@ public class Acciones : MonoBehaviour
         AnimacionAgrandar();
 
         ControlPantallaEnZona();
-    }
-
-    private void ControlPantallaEnZona()
-    {
-        if (zonaControl.jugadorEnZona && zonaControl.nombreZonaJugador == zonaControlada.name && pantalla != null)
-        {
-            pantalla.SetActive(true);
-        }
-        else if (!zonaControl.jugadorEnZona && pantalla != null)
-        {
-            pantalla.SetActive(false);
-        }
-    }
-
-    private void OnDrawGizmosSelected()
-    {
-        Gizmos.color = colorPunto; // Color del punto
-        Gizmos.DrawSphere(posicionObjeto, 0.01f); // Dibujar un punto en la posición deseada
     }
 }
